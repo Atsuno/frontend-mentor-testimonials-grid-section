f101282a3504bd35b166c175c57d2c61f28f3046
Atsuno <achiptsuno@gmil.com>
2023-09-02 13:52:48 +0700

    start

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        barlow: ["Barlow Semi Condensed", "sans-sarif"],
+      },
+      colors: {
+        primary: "hsl(263, 55%, 52%)",
+        secondly: "hsl(217, 19%, 35%)",
+        thirdly: "hsl(219, 29%, 14%)",
+        fourthly: "hsl(0, 0%, 100%)",
+        rear: "hsl(210, 46%, 95%)",
+        primarytext: "hsl(0, 0%, 81%)",
+        footerlink: "hsl(228, 45%, 44%)",
+      },
+    },
+  },
+  plugins: [],
+}
+
